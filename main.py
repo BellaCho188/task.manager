56bd02321ab38c9b7f1c507e4dd6e2846555b58b
isabella cho <isabellalauracho@gmail.com>
2024-10-28 12:30:49 -0400

    updated

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from operator import itemgetter #will help extract the date
+"""
+1. order date
+2. time 
+"""
+from operator import itemgetter #will help extract the date, currently doesn't do anything
 task = []
 from datetime import date
 from datetime import datetime
-#=====================================================
+#==============================================================
 def sort_date(): 
     #sorted_task = sorted(task,key=itemgetter(1)) # will sort by the second element
     for i, j in enumerate(task):#enumerate adds a counter as the key of the object
         print(i + 1, " ", j)
 
-<<<<<<< HEAD
 def add_task():
     user_task = input("Enter Your Task>>")
     user_due_date = input("Enter the due date (format: MM-DD-YYY)>>")
@@ -18,18 +21,17 @@ def add_task():
         print("Task was successfully added!")
 
     except ValueError: #if error occurs, will prompt with:
-        print("invalid date format, please use MM-DD-YYY")
-=======
+        print("invalid date format, please use MM-DD-YYY") #(Doesnt currently work)
+"""
 def new_task():
     user_task = input("Enter task: ")
     task.append(user_task)
-
+"""
 def note_task():
     view_task()
     select_task = int(input("Which task would you like to add a note to? "))
     note_task = str(input("Enter note: "))
     task[select_task-1] = task[select_task-1] +'  NOTE: '+ note_task
->>>>>>> ac1e3725fb1598ac31499d8d7cc66e72d886f4c7
 
 def view_task():
     sort_date()
@@ -55,14 +57,10 @@ def main():
     
     show_menu()
     while True:
-<<<<<<< HEAD
         choice = int(input("Enter Your Choice (as a number)>>"))
-=======
-        choice = int(input("Enter your choice (a number 1-5): "))
->>>>>>> ac1e3725fb1598ac31499d8d7cc66e72d886f4c7
 
         if choice == 1:
-            new_task()
+            add_task()
         elif choice == 2:
             view_task()
         elif choice == 3:
@@ -76,4 +74,3 @@ def main():
 
 
 main()
-
